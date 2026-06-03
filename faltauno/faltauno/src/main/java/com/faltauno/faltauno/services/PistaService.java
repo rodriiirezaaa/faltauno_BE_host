ce3ee23bcb6c97fca79d0d5b9a37c919e43d9aab
rodriiirezaaa <alvarezrezarodrigo@gmail.com>
2026-06-03 21:59:14 +0200

    Update PistaService.java

--- faltauno/faltauno/src/main/java/com/faltauno/faltauno/services/PistaService.java
+++ faltauno/faltauno/src/main/java/com/faltauno/faltauno/services/PistaService.java
@@ -13,16 +13,17 @@ public class PistaService {
 
     @Autowired
     private PistaRepository pistaRepository;
-
+    @Transactional
     public List<Pista> obtenerTodas() {
         return pistaRepository.findAll();
     }
 
     // Usamos el Enum Pista.EstadoPista en lugar de String
+    @Transactional
     public List<Pista> obtenerPorEstado(Pista.EstadoPista estado) {
         return pistaRepository.findByEstado(estado);
     }
-
+    @Transactional
     public Pista guardarPista(Pista pista) {
         // Asignamos el Enum PENDIENTE si no tiene estado
         if (pista.getEstado() == null) {
@@ -32,6 +33,7 @@ public class PistaService {
     }
 
     // Cambiamos el id a Long y el nuevoEstado al Enum
+    @Transactional
     public Pista cambiarEstado(Long id, Pista.EstadoPista nuevoEstado) {
         Optional<Pista> pistaOpt = pistaRepository.findById(id);
         if (pistaOpt.isPresent()) {
