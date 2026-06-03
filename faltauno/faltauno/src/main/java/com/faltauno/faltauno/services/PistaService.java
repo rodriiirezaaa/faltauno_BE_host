60ca8116c406c74c73118003ba3a52bc52b60c86
rodriiirezaaa <alvarezrezarodrigo@gmail.com>
2026-06-03 22:05:14 +0200

    Update PistaService.java

--- faltauno/faltauno/src/main/java/com/faltauno/faltauno/services/PistaService.java
+++ faltauno/faltauno/src/main/java/com/faltauno/faltauno/services/PistaService.java
@@ -4,7 +4,7 @@ import com.faltauno.faltauno.models.Pista;
 import com.faltauno.faltauno.repositories.PistaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 
