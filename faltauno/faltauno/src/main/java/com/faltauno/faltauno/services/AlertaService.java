002741f07940e45a3adbc078b89c348ef18a1a97
rodriiirezaaa <alvarezrezarodrigo@gmail.com>
2026-06-03 22:05:00 +0200

    Update AlertaService.java

--- faltauno/faltauno/src/main/java/com/faltauno/faltauno/services/AlertaService.java
+++ faltauno/faltauno/src/main/java/com/faltauno/faltauno/services/AlertaService.java
@@ -4,6 +4,7 @@ import com.faltauno.faltauno.models.Alerta;
 import com.faltauno.faltauno.repositories.AlertaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
